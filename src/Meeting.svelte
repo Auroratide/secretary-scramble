d6a785a62e9039b6fa080d9f4f00662fecd65fe2
Timothy Foster <tf.auroratide@gmail.com>
2022-04-02 10:53:08 -0500

    Add a couple of basic meetings

---
+++ src/Meeting.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let duration // minutes
+</script>
+
+<article style:height="{duration}px">
+    <h3>Meeting</h3>
+</article>
+
+<style>
+    article {
+        background: wheat;
+        margin-bottom: 10px;
+    }
+</style>
