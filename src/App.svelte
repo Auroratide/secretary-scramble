678d57045083d308adc9e3bf4e879642811eefce
Timothy Foster <tf.auroratide@gmail.com>
2022-04-02 11:03:16 -0500

    Start separating unscheduled and scheduled meetings

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,41 @@
 <script>
+	import { createUnscheduledMeeting, scheduleMeeting } from './game'
 	import Meeting from './Meeting.svelte'
+
+	let unscheduled = [
+		createUnscheduledMeeting({ duration: 60 }),
+		createUnscheduledMeeting({ duration: 30 }),
+	]
+
+	let scheduled = [
+		scheduleMeeting(createUnscheduledMeeting({ duration: 60 }))(0, 0),
+		scheduleMeeting(createUnscheduledMeeting({ duration: 30 }))(0, 120),
+	]
 </script>
 
 <main>
+	<div class="unscheduled">
+		{#each unscheduled as meeting (meeting.id)}
+			<Meeting duration={meeting.duration} />
+		{/each}
+	</div>
 	<div class="meetings">
-		<Meeting duration={60} />
-		<Meeting duration={30} />
+		{#each scheduled as meeting (meeting.id)}
+			<Meeting duration={meeting.duration} />
+		{/each}
 	</div>
 </main>
 
 <style>
+	main {
+		display: flex;
+		gap: 1em;
+	}
+
+	.unscheduled {
+		width: 600px;
+	}
+	
 	.meetings {
 		width: 600px;
 	}
--- src/game.js
+++ src/game.js
@@ -1,1 +1,17 @@
-// export const createMeeting = ()
+let meetingCounter = 0
+
+export const createUnscheduledMeeting = ({
+    duration, // minutes
+}) => ({
+    id: ++meetingCounter,
+    duration,
+})
+
+// 9am = 0; 5pm = 480
+export const scheduleMeeting = (meeting) => (day, minute) => ({
+    ...meeting,
+    start: {
+        day,
+        minute,
+    },
+})
